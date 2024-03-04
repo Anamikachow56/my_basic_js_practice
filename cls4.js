357e4e6471652b765d9f8299a0501d1956ee1f89
Anamikachow56 <157230828+Anamikachow56@users.noreply.github.com>
2024-03-04 11:24:39 +0600

    uoloaded 4rth file
    logical operator,loop

---
+++ cls4.js
@@ -0,0 +1,99 @@
+// Padma Meghna Jamuna
+// 
+// var riverList = ["padma" , "megna" , "jamuna"]
+// var numlist = [3,8,9,6]
+// console.log(numlist)
+// var sortednum = numlist.sort()
+// console.log("sorted array:" , sortednum)
+// console.log(riverlist) 
+// var sortedriver = riverList.sort()
+// console.log("sorted array :" , sortedriver)
+
+// var riverList = ["padma" , "megna" , "jamuna" , "yyu" , "gjhgj"]
+// var len = riverList.length
+// console.log("the lenght of array :" , len)
+// for( var i =0; i < len ; i++){
+//     console.log(riverList[i])
+// }
+// var landuse = ["2" , " 5675r" , "686"]
+// landuse [2] ="fyufuyfyifyfiyf"
+// for ( i = 0 ; i < landuse.length ; i++ ) {
+// console.log(landuse[i])}
+// var ar = ["hdiuhe" , "fhiuf" , "ehdwieh"]
+// ar [1] = "tduyetiyiewfgtwieugfiu" 
+// console.log( ar)
+// 
+// var a = []
+// a[0] = 2
+// a[1] = 3
+// a[2] = 4
+// for(i = 0 ; i < a.length ; i++ ) { 
+
+//     a.push =(a[i] + 1)
+// }
+
+// console.log15
+
+
+
+// 
+
+// prompt = require ("prompt-sync")();
+// a = []
+// for(i=0 ; i < 5; i++){
+//     var b = Number(prompt("enter a num : "))
+//     a.push(b)}
+//     console.log("without sorting the numbers are: " , a)
+//     console.log("with sorting the numbers are: " , a.sort())
+
+// prompt = require ("prompt-sync")();
+// var a =[]
+// var b =[]
+// for(i=0 ; i < 5; i++){
+//       var c = Number(prompt("enter a num : "))
+      
+// if (c % 2 == 0 )
+// a.push(c)
+// else
+// b.push(c)}
+// console.log("even number : " ,a)
+// console.log("odd number : " ,b)
+
+
+
+
+
+
+// prompt = require ("prompt-sync")();
+// var a =[]
+// var b =[]
+// for(i=0 ; i < 5; i++){
+//       var c = Number(prompt("enter a num : "))
+      
+// if (c % 2 == 0 )
+// a.push(c)
+// else
+// b.push(c)}
+// console.log("even number : " ,a)
+// console.log("odd number : " ,b)
+
+// var a = [[1 ,2] ,
+//          [3,4] ,
+//     
+//  var mat = [ [1, 2, 3],
+//              [4, 5, 6],
+//              [7, 8, 9] ]
+
+// console.log("The original matrix is: " , mat)
+// for(i = 0; i< mat.length; i++){
+// for(j=0; j< 3 ; j++){
+// mat[i][j]= mat [i][j] +1
+// }}
+//       console.log("increased matrix is : " ,mat)
+var a = [1,2,5,6,7
+      ,8,9,10,45,34,7]
+      a.sort();
+      console.log(a)
+// var prompt = require("prompt-sync")(); 
+// var a = prompt("enter a number: ");
+//  console.log("your entereed ", a)
